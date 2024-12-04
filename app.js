bce8ceb50a0541892ccd8d3476b25275a6365664
ATA AVCI <129445221+ataavci@users.noreply.github.com>
2024-12-04 15:36:34 +0300

    test 2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const sequelize = require("./db/db");
-const simulation_contoller=require("../myapp/contoller/simulation_contoller"); // Sequelize bağlantısı
+const simulation_contoller=require("../arkas_new/contoller/simulation_contoller"); // Sequelize bağlantısı
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
