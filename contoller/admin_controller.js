368aee281580fbb19af034c3267a774ea2c53641
ATA AVCI <129445221+ataavci@users.noreply.github.com>
2024-12-05 23:51:00 +0300

    input sayfası geldi

---
+++ contoller/admin_controller.js
@@ -0,0 +1,11 @@
+const dashboard_page_show= (req,res,next)=>{
+    res.render("admin/dashboard",{layout:"layout/admin_layout.ejs"})
+}
+const input_page_show= (req,res,next)=>{
+    res.render("admin/input",{layout:"layout/admin_layout.ejs"})
+}
+
+
+module.exports={
+    dashboard_page_show,input_page_show
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const admin_controller=require("../contoller/admin_controller");
 
-
+router.get("/admin/dashboard",admin_controller.dashboard_page_show);
+router.get("/admin/input",admin_controller.input_page_show)
 
 
 module.exports = router;
