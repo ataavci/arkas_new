da8f0773206c1c7354dfffb95bcd408993e27128
ATA AVCI <129445221+ataavci@users.noreply.github.com>
2024-12-14 19:53:53 +0300

    login ayarı

--- routes/office_route.js
+++ routes/office_route.js
@@ -9,19 +9,19 @@ const authMiddleware=require("../middleware/auth_middileware");
 
 
 router.get("/office/office_dashboard",authMiddleware.openup,admin_controller.dashboard_page_show);
-router.get("/office/office_input",admin_controller.input_page_show)
-router.post('/simulation', simulation_contoller.simulate);
-router.get('/countries',office.getCountries)
-router.post('/input', office.office_calculate);
-router.get('/office/scope-emissions',office.getScopePieCharts);
-router.get('/office/monthly-emissions',office.getMonthlyEmissions);
-router.get('/office/mobile-consumption', office.getMobileConsumptionData);
-router.get('/office/monthly-scopes', office.getMonthlyScopesData);
-router.get('/office/scope-data', office.getScopeData);
-router.get('/office/total-emission', office.getTotalEmission);
-router.get('/office/offset-carbon', office.getTotaloffset);
-router.get('/office/offset-percentage', office.getOffsetPercentage);
-router.get('/office/remaining-carbon', office.getRemainingCarbon);
+router.get("/office/office_input",authMiddleware.openup,admin_controller.input_page_show)
+router.post('/simulation',authMiddleware.openup, simulation_contoller.simulate);
+router.get('/countries',authMiddleware.openup,office.getCountries)
+router.post('/input',authMiddleware.openup, office.office_calculate);
+router.get('/office/scope-emissions',authMiddleware.openup,office.getScopePieCharts);
+router.get('/office/monthly-emissions',authMiddleware.openup,office.getMonthlyEmissions);
+router.get('/office/mobile-consumption',authMiddleware.openup, office.getMobileConsumptionData);
+router.get('/office/monthly-scopes',authMiddleware.openup, office.getMonthlyScopesData);
+router.get('/office/scope-data', authMiddleware.openup,office.getScopeData);
+router.get('/office/total-emission',authMiddleware.openup, office.getTotalEmission);
+router.get('/office/offset-carbon',authMiddleware.openup, office.getTotaloffset);
+router.get('/office/offset-percentage',authMiddleware.openup, office.getOffsetPercentage);
+router.get('/office/remaining-carbon', authMiddleware.openup,office.getRemainingCarbon);
 
 
 module.exports = router;
