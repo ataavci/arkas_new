4a0038171c911301697d3313fd7e899bb00e98f5
ATA AVCI <129445221+ataavci@users.noreply.github.com>
2024-12-16 17:41:12 +0300

    sıfır sıkıntı

--- db/passport_local.js
+++ db/passport_local.js
@@ -35,15 +35,22 @@ passport.use(
 );
 
 passport.serializeUser((user, done) => {
+    console.log("Serialize User ID:", user.id); // Serialize edilen ID'yi kontrol edin
     done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
     try {
-        const user = await USER.findByPk(id); // Kullanıcıyı veritabanından bul
+        const userId = id.id || id; // Gelen ID'nin obje ya da düz bir değer olup olmadığını kontrol edin
+        const user = await USER.findByPk(userId);
+        if (!user) {
+            console.error("User not found for ID:", userId);
+            return done(null, false);
+        }
         done(null, user);
-    } catch (error) {
-        done(error, null);
+    } catch (err) {
+        console.error("Error in deserializeUser:", err);
+        done(err, null);
     }
 });
 
