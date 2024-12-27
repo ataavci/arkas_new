ac90e4aeb0a3ea4df3526c0f924ebf32a5fdfe91
ATA AVCI <129445221+ataavci@users.noreply.github.com>
2024-12-27 19:10:43 +0300

    tasarım ayakalndı

--- routes/payment_route.js
+++ routes/payment_route.js
@@ -4,5 +4,9 @@ const { payment } = require('../controller/payment_controller');
 
 
 router.post("/api/payment",payment)
+router.get('/payment', (req, res) => {
+    res.render("payment/payment", { layout: false });
+});
+
 
 module.exports = router;
